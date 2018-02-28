97b0f6de3e2d14bd0206f4cb48605a8355561994
Sung Jae Hong <sroyhong313@gmail.com>
2018-02-28 14:51:35 -0500

    add db model and router ops

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,9 +2,57 @@
 
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var Expense = require('../../models/Expense');
 
 router.get('/', function(req, res) {
     res.render('index')
+});
+
+router.route('/insert')
+.post(function(req, res) {
+    var expense = new Expense();
+    expense.description = req.body.desc;
+    expense.amount = req.body.amount;
+    expense.month = req.body.month;
+    expense.year = req.body.year;
+
+    expense.save(function(err) {
+        if (err) {
+            res.send(err);
+        }
+        res.send('Expense successfully added!');
+    });
+});
+
+router.route('/update')
+.post(function(req, res) {
+    const doc = {
+        description : req.body.description,
+        amount : req.body.amount,
+        month : req.body.month,
+        year : req.body.year
+    };
+    console.log(docs);
+    Expense.update({ _id : req.body._id}, doc, function(err, result) {
+        if (err) {
+            res.send(err);
+        }
+        res.send('Expense successfully updated!');
+    });
+});
+
+router.get('getAll', function(req, res) {
+    var monthRec = req.query.month;
+    var yearRec = requ.query.year;
+    if (monthRec && monthRec != 'All') {
+        Expense.find({ year : yearRec}, function(err, expenses) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(expenses)
+        });
+    }
 });
 
 module.exports = router;
