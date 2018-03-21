de5c1a2a4d15776a9f37f442a68075c979ce6a82
Sung Jae Hong <sroyhong313@gmail.com>
2018-03-21 15:47:31 -0400

    add year tabs

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,11 @@
 // clients/components/App.js
 
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import '../css/App.css';
+import {Tab, Tabs} from 'react-bootstrap';
+import YearTabsRouter from './tabs/yearTabsRouter';
 
 import Add from './Add';
 import Update from './Update';
@@ -15,31 +17,63 @@ class App extends React.Component {
     this.state = {
       selectedMonth : 'All',
       selectedYear : 2016,
-      data : []
+      data : [],
+      activeTab : 2016
     };
     this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
-    this.getData(this, '2016');
+    this.getData(this, 2016, 'All');
   }
 
+  /*
+   * we will need to render this component in the App component and fetch the data from the server
+   * using the prop sent from YearTabsRouter. The new props will be received in the componentWillReceiveProps()
+   * method. We will make a call to the server and reload the table using the year received as a prop
+   */
   componentWillReceiveProps(nextProps) {
-    this.getData(this, '2016');
+    if (nextProps.history.location.search) {
+      var search = nextProps.history.location.search;
+      search = search.substring(1);
+      var searchObj = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"')
+                                 .replace(/&/g, '", "').replace(/=/g, '":"') + '"}');
+      this.setState({ activeTab : parseInt(searchObj.year) })
+      this.setState({ selectedYear : parseInt(searchObj.year) })
+      this.setState({ selectedMonth : parseInt(searchObj.month) })
+      this.getData(this, searchObj.year, searchObj.month);
+    } else {
+      this.getData(this, 2016, 'All');
+    }
+  }
+
+  handleSelect(selectedTab) {
+    this.setState({
+      activeTab : selectedTab,
+      selectedYear : selectedTab
+    });
   }
 
   // get all expenses from database using /getAll
-  getData(ev, year) {
-    axios.get('/getAll?month=All&year=' + year)
+  getData(ev, year, month) {
+    axios.get('/getAll?month=' + month + '&year=' + year)
       .then(function(resp) {
         ev.setState({ data : resp.data });
-        ev.setState({ selectedYear : parseInt(year) })
+        ev.setState({ selectedYear : parseInt(year) });
+        ev.setState({ selectedMonth : parseInt(month) });
       });
   }
 
   render() {
     return (
       <div>
+        <Tabs activeKey={this.state.activeTab} onSelect={this.handleSelect}>
+          <Tab eventKey={2016} title={<YearTabsRouter year='2016' />}></Tab>
+          <Tab eventKey={2017} title={<YearTabsRouter year='2017' />}></Tab>
+          <Tab eventKey={2018} title={<YearTabsRouter year='2018' />}></Tab>
+          <Tab eventKey={2019} title={<YearTabsRouter year='2019' />}></Tab>
+          <Tab eventKey={2020} title={<YearTabsRouter year='2020' />}></Tab>
+        </Tabs>
         <Add selectedMonth={this.state.selectedMonth} selectedYear={this.state.selectedYear} />
           <table>
             <thead>
---
+++ client/components/tabs/yearTabsRouter.js
@@ -0,0 +1,31 @@
+// client/components/tabs/yearTabsRouter.js
+
+/* returns a Link that renders the App component
+   (using the route we created earlier) with year
+   sent in as a prop in search. Whenever this link will be clicked,
+   App will be rendered and the expenses of the year sent in search will be loaded
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Tab, Tabs } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+class YearTabsRouter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            style : { 'font-size' : '16px'}
+        }
+    }
+
+    render() {
+        return (
+            <Link to= {{ pathname : '/', search : '?month=All&year='+ this.props.year }}>
+                <p style={this.state.style}>{this.props.year}</p>
+            </Link>
+        )
+    }
+}
+
+export default YearTabsRouter;
