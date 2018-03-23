b560ee241c9958990d926040f545d53128ce196b
Sung Jae Hong <sroyhong313@gmail.com>
2018-03-23 10:46:09 -0400

    add .env config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
@@ -14,7 +18,13 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit : '50mb'}));
 app.use(bodyParser.urlencoded({ limit : '50mb', extended : false }));
 
-mongoose.connect('mongodb://admin:admin_pass@ds151508.mlab.com:51508/expenses')
+mongoose.connect(process.env.DB_URL);
 app.use('/', router);
+
+PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function() {
+    console.log("Running @ localhost : " + PORT)
+});
 
 module.exports = app;
